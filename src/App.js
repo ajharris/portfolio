46207c76af87a57d0ab69a93e4530ba702ca9ed7
Andrew Harris <aharris.live@gmail.com>
2025-01-21 13:32:29 -0500

    initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import './components/Projects'
+import Projects from './components/Projects';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Andrew Harris</h1>
+        <h2>Portfolio</h2>
+        <p>andrew.harris (at) torontomu.ca</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Projects.js
@@ -0,0 +1,5 @@
+// Projects component will render a drop-down list of current and past projects, which will generate links to the hosted version as well as the Github for the project
+
+export default function Projects(){
+    
+}
